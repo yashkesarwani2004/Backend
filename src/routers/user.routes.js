d389855c2e7230731130b6d4c277169c8fbf218a
yashkesarwani2004 <yashkesarwani2004@gmail.com>
2026-09-08 11:29:01 +0530

    Add more routers

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -1,31 +1,50 @@
 import { Router } from "express";
-import { registerUser,loginUser,logoutUser,refreshAccessToken } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    updateAccountDetails
+} from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 const router = Router()
-
 
 router.route("/register").post(
     upload.fields([
         {
             name: "avatar",
             maxCount: 1
-        },
+        }, 
         {
             name: "coverImage",
             maxCount: 1
         }
     ]),
     registerUser
-) //jab app se yaha aayga fir koi /register karega tab ye chalega
+    )//jan aap se yaha aayga /register karega koi tab ye chalega
 
 router.route("/login").post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJWT,  logoutUser)
-
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails) //patch bas kuch chij ko uupdate karta hai iss liye yaha liya kyuki post ekdum naya deta hai
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
